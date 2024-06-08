10ffc692cc46175e371687f067860c5247bba418
gregothedeveloper <gregoforty2@gmail.com>
2024-06-08 11:48:09 +0300

    modifications

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".slide-content", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
